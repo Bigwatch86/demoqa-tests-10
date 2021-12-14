25c317b4c4609686c8dd337cfecd521f55ca313d
Bigwatch86 <bigwatch86@gmail.com>
2021-12-14 15:53:37 +0300

    added FirstTest.java

---
+++ src/test/java/online/fortis/FirstTest.java
@@ -0,0 +1,17 @@
+package online.fortis;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class FirstTest {
+
+
+
+    @Test
+    void firstTest(){
+        Assertions.assertTrue(true);
+
+    }
+}
