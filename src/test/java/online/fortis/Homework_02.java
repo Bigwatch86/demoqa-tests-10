d3b30cf2a917c1601e726f314ef081963d780712
Bigwatch86 <bigwatch86@gmail.com>
2021-12-15 18:25:31 +0300

    added homework_02 corrections 2

--- src/test/java/online/fortis/Homework_02.java
+++ src/test/java/online/fortis/Homework_02.java
@@ -1,11 +1,14 @@
 package online.fortis;
 
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -37,21 +40,35 @@ public class Homework_02 {
         $(".react-datepicker__day--012").click();
     // заполняем subjects
         $("#subjectsInput").setValue("English").pressEnter();
+        $("#subjectsInput").setValue("Computer Science").pressEnter();
     // выбираем хобби
         $("[for=hobbies-checkbox-1]").click();
         $("[for=hobbies-checkbox-3]").click();
     // загружаем файл
-        File file = new File("src/test/resources/Glazov.txt");
+        File file = new File("src/test/Glazov.txt");
         $("input[id='uploadPicture']").uploadFile(file);
     // заполняем адрес
-        $("#currentAddress").setValue("СurrentAddress");
+        $("#currentAddress").setValue("Any Address");
     // выбираем штат
-        $(". css-tlfecz-indicatorContainer").click();
-        //$("#react-select-3-input").setValue("Haryana");
+        $("#react-select-3-input").setValue("Haryana").pressEnter();
     // выбираем город
-        $("#react-select-4-input").setValue("Karnal");
+        $("#react-select-4-input").setValue("Karnal").pressEnter();
     // нажимаем submit
-        //$("#submit").click();
+        $("#submit").click();
+
+    // Проверка результата
+        $("#example-modal-sizes-title-lg").shouldBe(visible);
+        $(".table-responsive").shouldHave(text("FirstName"));
+        $(".table-responsive").shouldHave(text("LastName"));
+        $(".table-responsive").shouldHave(text("test@test.ru"));
+        $(".table-responsive").shouldHave(text("Other"));
+        $(".table-responsive").shouldHave(text("9991234567"));
+        $(".table-responsive").shouldHave(text("12 August,1986"));
+        $(".table-responsive").shouldHave(text("English, Computer Science"));
+        $(".table-responsive").shouldHave(text("Sports, Music"));
+        $(".table-responsive").shouldHave(text("Glazov.txt"));
+        $(".table-responsive").shouldHave(text("Any Address"));
+        $(".table-responsive").shouldHave(text("Haryana Karnal"));
 
     }
 }
